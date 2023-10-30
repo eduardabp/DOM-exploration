82ea5158b7119859559d43384f9a38de48d44811
Eduarda <eduardabpetry@gmail.com>
2023-10-30 16:45:15 +0100

    Add randomize effect on page load

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,7 +1,24 @@
-const students = ["Jana", "Becca", "Jonasi", "Pieter", "Alex", "Alessandro", "Josué"]
+const students = ["Jana", "Becca", "Jonasi", "Pieter", "Alex", "Alessandro", "Josué"];
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+
+        // Generate random number 
+        var j = Math.floor(Math.random() * (i + 1));
+
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
+
+shuffleArray(students);
 
 students.forEach(student => {
-    let table = document.createElement("section"); 
+    let table = document.createElement("section");
+
     let r = Math.floor(Math.random() * 254);
     let g = Math.floor(Math.random() * 254);
     let b = Math.floor(Math.random() * 254);
