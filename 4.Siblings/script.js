bb32206894a2522389a4521573c53c53a0af7d7f
Eduarda <eduardabpetry@gmail.com>
2023-10-31 10:41:55 +0100

    Change list order and remove last section

--- 4.Siblings/script.js
+++ 4.Siblings/script.js
@@ -1,8 +1,13 @@
+const list = document.querySelector('ol');
+
+list.insertBefore(list.children[4], list.children[0]);
+
 const main = document.querySelector('main');
 
 const sectionTwo = main.children[1];
 const sectionThree = main.children[2];
 
-
 sectionTwo.insertBefore(sectionThree.children[0].children[0], sectionTwo.children[0]);
 sectionThree.insertBefore(sectionTwo.children[1], sectionThree.children[0]);
+
+sectionThree.remove();
