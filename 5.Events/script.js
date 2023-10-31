657345f20af65878b71960ca099128a99eb2f29b
Eduarda <eduardabpetry@gmail.com>
2023-10-31 11:59:48 +0100

    Generate new square and log time and color in the list

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -7,10 +7,23 @@ const getElapsedTime = () => {
 const clickOnSquare = (e) => {
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
+
+  const newSquare = document.createElement("div");
+  newSquare.classList.add("displayedsquare");
+  newSquare.classList.add(e.target.classList[1]);
+
+  const parent = document.querySelector(".displayedsquare-wrapper");
+  parent.appendChild(newSquare);
+
+  const logList = document.querySelector("ul");
+  const logItem = document.createElement("li");
+  const logMessage = document.createTextNode(getElapsedTime() + " to generate a " + e.target.classList[1] + " square");
+
+  logItem.appendChild(logMessage);
+  logList.appendChild(logItem);
 }
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
-
