243d75d3ffd7043da4d1377b984ec24c46a6da48
Eduarda <eduardabpetry@gmail.com>
2023-10-31 16:35:53 +0100

    Add check age function and event listener

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -6,8 +6,16 @@ const typeName = (e) => {
     span.appendChild(letter);
 }
 
-
-
 const firstName = document.getElementById("firstname");
 firstName.addEventListener('keyup', typeName);
+
+const checkAge = (e) => {
+    if (e.target.value >= 18) {
+        let quote = document.getElementById("a-hard-truth");
+        quote.style.visibility = "visible";
+    }
+}
+
+const age = document.getElementById("age");
+age.addEventListener('keyup', checkAge);
 
