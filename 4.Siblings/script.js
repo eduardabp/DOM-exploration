c1beacbfc0ccabbbf08f4ff60583341ce04ed787
Eduarda <eduardabpetry@gmail.com>
2023-10-31 10:29:54 +0100

    Move headings between section 2 and 3

---
+++ 4.Siblings/script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('main');
+
+const sectionTwo = main.children[1];
+const sectionThree = main.children[2];
+
+
+sectionTwo.insertBefore(sectionThree.children[0].children[0], sectionTwo.children[0]);
+sectionThree.insertBefore(sectionTwo.children[1], sectionThree.children[0]);
