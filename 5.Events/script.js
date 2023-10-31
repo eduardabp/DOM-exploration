38486008620ad36c70739ab6abe8d64a1dd30f06
Eduarda <eduardabpetry@gmail.com>
2023-10-31 15:26:04 +0100

    Create key events for body

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -27,3 +27,38 @@ const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
+
+const keyEvent = (e) => {
+  if (e.code == "Space") {
+    let r = Math.floor(Math.random() * 254);
+    let g = Math.floor(Math.random() * 254);
+    let b = Math.floor(Math.random() * 254);
+    let randomColor = "rgb(" + r + ", " + g + ", " + b + ")";
+    body.style.backgroundColor = randomColor;
+
+    const logList = document.querySelector("ul");
+    const logItem = document.createElement("li");
+    const logMessage = document.createTextNode("After " + getElapsedTime() + " someone used the spacebar");
+  
+    logItem.appendChild(logMessage);
+    logList.appendChild(logItem);
+  }
+
+  else if (e.code == "KeyL") {
+    const list = document.getElementsByTagName("li");
+    while (list.length > 0) {
+      list[0].remove();
+    }
+  }
+  else if (e.code == "KeyS") {
+    const squares = document.getElementsByClassName("displayedsquare");
+    while (squares.length > 0) {
+      squares[0].remove();
+    }
+  }
+}
+
+const body = document.querySelector("body");
+body.addEventListener("keyup", keyEvent);
+
+
