5aa2ab32076fb8baca33a3027f2e9b265ce95f17
Eduarda <eduardabpetry@gmail.com>
2023-11-06 13:59:13 +0100

    Add dark mode to cart

--- Projects/2.GhostKitchen/script.js
+++ Projects/2.GhostKitchen/script.js
@@ -50,7 +50,9 @@ menu.forEach(dish => {
 const toggle = document.getElementById("toggle");
 toggle.addEventListener('click', () => {
   const main = document.querySelector("main");
+  const cart = document.getElementById("cart");
   main.classList.toggle('dark-mode');
+  cart.classList.toggle('dark-mode');
 });
 
 const btnCart = document.querySelector('#cart-icon');
@@ -94,17 +96,16 @@ function loadContent(){
 
 //Remove Item
 function removeItem(){
-  if(confirm('Are Your Sure to Remove')){
     let title = this.parentElement.querySelector('.cart-food-title').innerHTML;
     itemList = itemList.filter(el=>el.title!=title);
     this.parentElement.remove();
     loadContent();
-  }}
+  }
 
 //Change Quantity
 function changeQty(){
   if(isNaN(this.value) || this.value<1){
-    this.value=1;
+    this.value = 1;
   }
   loadContent();
 }
@@ -112,10 +113,10 @@ let itemList = [];
 
 //Add Cart
 function addCart(){
- let food=this.parentElement;
- let title=food.querySelector('.food-title').innerHTML;
- let price= "€" + food.querySelector('.food-price').innerHTML;
- let imgSrc=food.querySelector('.food-img').src;
+ let food = this.parentElement;
+ let title = food.querySelector('.food-title').innerHTML;
+ let price = "€" + food.querySelector('.food-price').innerHTML;
+ let imgSrc = food.querySelector('.food-img').src;
  let newProduct={title,price,imgSrc};
 
  //Check Product already Exist in Cart
@@ -126,10 +127,10 @@ function addCart(){
   itemList.push(newProduct);
  }
 
-let newProductElement= createCartProduct(title,price,imgSrc);
-let element=document.createElement('div');
-element.innerHTML=newProductElement;
-let cartBasket=document.querySelector('.cart-content');
+let newProductElement = createCartProduct(title,price,imgSrc);
+let element = document.createElement('div');
+element.innerHTML = newProductElement;
+let cartBasket = document.querySelector('.cart-content');
 cartBasket.append(element);
 loadContent();
 }
@@ -152,32 +153,26 @@ function createCartProduct(title,price,imgSrc){
 }
 
 function updateTotal () {
-  const cartItems=document.querySelectorAll('.cart-box');
-  const totalValue=document.querySelector('.total-price');
+  const cartItems = document.querySelectorAll('.cart-box');
+  const totalValue = document.querySelector('.total-price');
 
   let total=0;
 
   cartItems.forEach (product => {
-    let priceElement=product.querySelector('.cart-price');
-    let price=parseFloat(priceElement.innerHTML.replace("€",""));
+    let priceElement = product.querySelector('.cart-price');
+    let price = parseFloat(priceElement.innerHTML.replace("€",""));
     console.log(priceElement.innerHTML);
     console.log(price);
-    let qty=product.querySelector('.cart-quantity').value;
+    let qty = product.querySelector('.cart-quantity').value;
     total+=(price*qty);
-    product.querySelector('.cart-amt').innerText="€"+(price*qty);
+    product.querySelector('.cart-amt').innerText = "€"+(price*qty);
   });
 
-  totalValue.innerHTML='€'+total;
+  totalValue.innerHTML = '€'+total;
 
   // Add Product Count in Cart Icon
 
-  const cartCount=document.querySelector('.cart-count');
-  let count=itemList.length;
-  cartCount.innerHTML=count;
-
-  if(count==0){
-    cartCount.style.display='none';
-  }else{
-    cartCount.style.display='block';
-  }
+  const cartCount = document.querySelector('.cart-count');
+  let count = itemList.length;
+  cartCount.innerHTML = count;
 }
